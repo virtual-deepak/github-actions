609803c70796beb36290d159f96de2e28786e3ec
Deepak Agarwal <deep.agarwal@hotmail.com>
2023-02-23 13:33:27 +0530

    Added sample-mvc-app

---
+++ sample-mvc-app/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace sample_mvc_app.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
