32651bd743a69dcb52596d693c2c38b769ae8e68
Deepak Agarwal <deep.agarwal@hotmail.com>
2023-02-25 18:48:59 +0530

    Added sample-mvc-docker-app

---
+++ sample-mvc-docker-app/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace sample_mvc_docker_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
