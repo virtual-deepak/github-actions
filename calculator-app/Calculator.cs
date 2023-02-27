4026351b8c529af5afe32325264ceeefd6e671e3
Deepak Agarwal <deep.agarwal@hotmail.com>
2023-02-27 22:00:26 +0530

    Added calculator app and tests with workflow

---
+++ calculator-app/Calculator.cs
@@ -0,0 +1,15 @@
+﻿namespace CalculatorApp
+{
+    public class Calculator
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+        
+        public int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
---
+++ calculator-app/CalculatorTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalculatorApp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace calculator_app
+{
+    [TestClass]
+    internal class CalculatorTests
+    {
+        [TestMethod]
+        public void TestAdd()
+        {
+            Calculator calculator = new();
+            Assert.Equals(calculator.Add(3, 4), 7);
+        }
+
+        [TestMethod]
+        public void TestMultiply()
+        {
+            Calculator calculator = new();
+            Assert.Equals(calculator.Multiply(3, 4), 12);
+        }
+
+    }
+}
